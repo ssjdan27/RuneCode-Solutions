d6bd21343f4c88b7672d32c3394287b2cee4db8a
Daniel Garza <danielg272313@gmail.com>
2026-04-02 14:22:44 -0600

    Add accepted solution for The Merchant’s Resource Allocation (cmnhx8pg600fmp919sjxnjjt3)

---
+++ runecode-solutions/the-merchant-s-resource-allocation-mngvfznh/cmnhx8pg600fmp919sjxnjjt3.py
@@ -0,0 +1,26 @@
+def max_potency_with_budget(N, B, potency, cost):
+
+    start = 0
+    max_potency = 0
+    current_cost = 0
+    current_potency = 0
+
+    for end in range(N):
+        current_cost += cost[end]
+        current_potency += potency[end]
+
+        while current_cost > B:
+            current_cost -= cost[start]
+            current_potency -= potency[start]
+            start += 1
+
+        max_potency = max(max_potency, current_potency)
+
+    return max_potency
+# Input
+N, B = map(int, input().split())
+potency = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+
+# Output
+print(max_potency_with_budget(N, B, potency, cost))
