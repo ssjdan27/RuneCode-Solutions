d3455b82c0c30bfd45e610adebde0326dc72eb81
Daniel Garza <danielg272313@gmail.com>
2026-04-02 14:17:08 -0600

    Add accepted solution for The Adventurer's Satchel (cmnhx1huo0072p919czv61tn0)

---
+++ runecode-solutions/the-adventurer-s-satchel-mnhlfp94/cmnhx1huo0072p919czv61tn0.py
@@ -0,0 +1,17 @@
+def main():
+    n, w = map(int, input().split())
+    weights = list(map(int, input().split()))
+    powers = list(map(int, input().split()))
+    # dp[w] = max power achievable with capacity w
+    dp = [0] * (w + 1)
+    for i in range(n):
+        wi = weights[i]
+        pi = powers[i]
+        # Iterate in reverse to preserve the 0/1 constraint
+        for cap in range(w, wi - 1, -1):
+            if dp[cap - wi] + pi > dp[cap]:
+                dp[cap] = dp[cap - wi] + pi
+    print(dp[w])
+def main_entry():
+    main()
+main_entry()
