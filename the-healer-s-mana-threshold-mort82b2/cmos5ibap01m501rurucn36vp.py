379f15301c9004e4e3897ab6103e752222408258
Daniel Garza <danielg272313@gmail.com>
2026-05-04 22:51:38 -0600

    Add accepted solution for The Healer's Mana Threshold (cmos5ibap01m501rurucn36vp)

---
+++ the-healer-s-mana-threshold-mort82b2/cmos5ibap01m501rurucn36vp.py
@@ -0,0 +1,23 @@
+import sys
+
+def main():
+    data = sys.stdin.read().strip().split()
+    it = iter(data)
+
+    n = int(next(it))
+    k = int(next(it))
+
+    running_sum = 0
+
+    for i in range(1, n + 1):
+        x = int(next(it))
+        running_sum += x
+
+        if running_sum == k:
+            print(i)
+            return
+
+    print(-1)
+
+if __name__ == "__main__":
+    main()
